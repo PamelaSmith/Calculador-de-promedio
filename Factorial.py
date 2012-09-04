73680c1d4a28a4ef401f484955ce62412d4769d2
Pamela Smith <pamelasmith04@hotmail.com>
2012-09-03 22:06:50 -0400

    ...

--- Factorial.py
+++
@@ -1,6 +0,0 @@
-num = int(raw_input("Digite un numero para encontrar el factorial : "))
-n = 1
-while num >= 1:
-    n = n * num
-    num -= 1
-print "El factorial del numero introducido es ",n
--- README.md
+++
@@ -1,9 +0,0 @@
-nu = int(raw_input('Cuantos numeros? '))
-
-su = 0.0
-for i in range(nu):
-    x = float(raw_input())
-    su = su + x
-
-prom = su / nu
-print 'El promedio es' , prom
